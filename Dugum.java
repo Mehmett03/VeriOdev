812156ad7f1c7c79f178742ae87f8f794dee4846
MEHMET AKGÜNGÖR <99881339+Mehmett03@users.noreply.github.com>
2022-11-10 23:10:48 +0300

    Add files via upload

---
+++ Dugum.java
@@ -0,0 +1,16 @@
+
+public class Dugum {
+
+    int index;
+    String kelime;
+
+    public Dugum(int index, String kelime) {
+        this.index = index;
+        this.kelime = kelime;
+    }
+
+    public Dugum() {
+
+    }
+
+}
